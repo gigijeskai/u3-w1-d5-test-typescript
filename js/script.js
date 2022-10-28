0a4c0d0e3781b2128503e6cd9271a565dfd8ecda
Luigi Indaco <luigindaco@gmail.com>
2022-10-28 11:27:50 +0200

    creazione classe

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 "use strict";
+class Smartphone {
+    constructor(carica, numeroChiamate) {
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;
+    }
+    chiamata(durata) {
+        this.numeroChiamate++;
+        this.carica -= durata * 0.2;
+        //deve aggiungere la chiamata a numero chiamate
+        //deve scalare il credito da carica per 0.20cent per ogni minuto di durata
+    }
+    numero404() {
+        return this.carica;
+        // deve restituire il valore della carica
+    }
+    getNumeroChiamate() {
+        return this.numeroChiamate;
+        // deve restituire il numero di chiamate
+    }
+    azzeraChiamate() {
+        return (this.numeroChiamate = 0);
+        // deve azzerare le chiamate
+    }
+}
---
+++ ts/script.ts
@@ -0,0 +1,26 @@
+class Smartphone {
+  carica: number;
+  numeroChiamate: number;
+  constructor(carica: number, numeroChiamate: number) {
+    this.carica = carica;
+    this.numeroChiamate = numeroChiamate;
+  }
+  chiamata(durata: number) {
+    this.numeroChiamate++;
+    this.carica -= durata * 0.2;
+    //deve aggiungere la chiamata a numero chiamate
+    //deve scalare il credito da carica per 0.20cent per ogni minuto di durata
+  }
+  numero404() {
+    return this.carica;
+    // deve restituire il valore della carica
+  }
+  getNumeroChiamate() {
+    return this.numeroChiamate;
+    // deve restituire il numero di chiamate
+  }
+  azzeraChiamate() {
+    return (this.numeroChiamate = 0);
+    // deve azzerare le chiamate
+  }
+}
